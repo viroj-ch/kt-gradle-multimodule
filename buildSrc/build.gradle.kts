6f8632129158d7e79e08d9852ba5a38e496adf5f
roj <viroj.chaipredanan@krungsri.com>
2022-10-19 16:12:31 +0700

    upgrade kotlin and spring boot version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.7.20")
 
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.3")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.4")
 }
