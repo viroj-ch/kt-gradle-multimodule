0e92da69f486ad2654029ca2395fbd7de4c53021
roj <viroj.chaipredanan@krungsri.com>
2022-10-20 17:55:29 +0700

    add dependency [ktlint] aims to Kotlin coding conventions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.7.20")
 
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.7.4")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
 }
